06f6c4266f6f8127d53687dfd526cbe8d986cb5d
Lucas Fernandes <lucas.almeida@gbarbosa.com.br>
2023-07-14 18:19:46 -0300

    continuacao Estrategia PEPS par2

--- Estrategia PEPS/FN_EXCEDENTE_ENT.sql
+++ Estrategia PEPS/FN_EXCEDENTE_ENT.sql
@@ -79,7 +79,7 @@ CREATE OR REPLACE TYPE USRASSISTENTEFISCAL.T_EXCEDENTE_ENT  AS TABLE OF USRASSIS
 	END;
 
 */
-CREATE OR REPLACE FUNCTION USRASSISTENTEFISCAL.FN_EXCEDENTE_ENT(P_FILIAL IN VARCHAR2, P_DATA_VENDA IN DATE, P_MERC_CODIGO IN VARCHAR)
+CREATE OR REPLACE FUNCTION USRASSISTENTEFISCAL.FN_EXCEDENTE_ENT(P_FILIAL IN VARCHAR2, P_DATA_VENDA IN DATE, P_DATA_CORTE IN DATE, P_MERC_CODIGO IN VARCHAR)
 RETURN USRASSISTENTEFISCAL.T_EXCEDENTE_ENT
  IS
   V_RET USRASSISTENTEFISCAL.T_EXCEDENTE_ENT;  
@@ -336,50 +336,67 @@ BEGIN
 									   TO_CHAR(NFE.DT_FATO_GERADOR_IMPOSTO,'MMYYYY') MES_ANO_ARQUIVO,
 									   0 AS ATUALIZAR_ESTOQUE
 									FROM SYNCHRO.COR_DOF NFE
-									INNER JOIN SYNCHRO.COR_IDF NEI                          ON   NEI.DOF_ID   = NFE.ID
-									INNER JOIN SYNCHRO.TBFILIAL F                           ON  (F.EST_CODIGO = NFE.INFORMANTE_EST_CODIGO  OR F.FILIAL = NFE.INFORMANTE_EST_CODIGO)
-									LEFT JOIN USRASSISTENTEFISCAL.EXCEDENTE_MG_ENTRADA_DET U  ON   NFE.DOF_IMPORT_NUMERO = U.DOF_IMPORT_NUMERO AND NEI.MERC_CODIGO = U.MERC_CODIGO AND NEI.IDF_NUM = U.IDF_NUM
-									LEFT  JOIN SYNCHRO.GBA_ASSIST_CFOP C                    ON   NEI.CFOP_CODIGO = C.CFOP
-									LEFT  JOIN SYNCHRO.COR_MERCADORIA ME                    ON  (NEI.MERC_CODIGO = ME.MERC_CODIGO)
+									INNER JOIN SYNCHRO.COR_IDF NEI                           ON   NEI.DOF_ID   = NFE.ID
+									INNER JOIN SYNCHRO.TBFILIAL F                            ON  (F.EST_CODIGO = NFE.INFORMANTE_EST_CODIGO  OR F.FILIAL = NFE.INFORMANTE_EST_CODIGO)
+									LEFT JOIN USRASSISTENTEFISCAL.EXCEDENTE_MG_ENTRADA_DET U ON   NFE.DOF_IMPORT_NUMERO = U.DOF_IMPORT_NUMERO AND NEI.MERC_CODIGO = U.MERC_CODIGO AND NEI.IDF_NUM = U.IDF_NUM 
+									LEFT  JOIN SYNCHRO.GBA_ASSIST_CFOP C                     ON   NEI.CFOP_CODIGO = C.CFOP
+									LEFT  JOIN SYNCHRO.COR_MERCADORIA ME                     ON  (NEI.MERC_CODIGO = ME.MERC_CODIGO)
 									--LEFT  JOIN synchro.cor_unidade_mercadoria MU            ON  (ME.MERC_CODIGO  = MU.merc_codigo AND NEI.ENTSAI_UNI_CODIGO = MU.UNI_CODIGO)
-									LEFT  JOIN SYNCHRO.COR_PESSOA P                         ON   P.PFJ_CODIGO    = NFE.emitente_pfj_codigo
-									LEFT  JOIN SYNCHRO.COR_PESSOA_VIGENCIA PV               ON  (P.PFJ_CODIGO    = PV.PFJ_CODIGO AND PV.DT_FIM IS NULL)
+									LEFT  JOIN SYNCHRO.COR_PESSOA P                          ON   P.PFJ_CODIGO    = NFE.emitente_pfj_codigo
+									LEFT  JOIN SYNCHRO.COR_PESSOA_VIGENCIA PV                ON  (P.PFJ_CODIGO    = PV.PFJ_CODIGO AND PV.DT_FIM IS NULL)
 									WHERE
-									  F.FILIAL                       	= P_FILIAL
-									  AND NFE.DT_FATO_GERADOR_IMPOSTO  <= P_DATA_VENDA
-									  AND NEI.MERC_CODIGO            	= P_MERC_CODIGO
-									  AND NFE.CTRL_SITUACAO_DOF      	IN ('N','B')					  
-									  AND 1 =
-										(
-										  CASE
-											  WHEN NEI.CFOP_CODIGO = '1.403'                                          										THEN 1
-											  WHEN NEI.CFOP_CODIGO = '1.910'                                        										THEN 1
-											  WHEN NEI.CFOP_CODIGO = '1.949' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.403'                                        										THEN 1
-											  WHEN NEI.CFOP_CODIGO = '1.409'                                        										THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.409' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.910' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.923' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.949' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '3.102' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '70')               			THEN 1
-											  WHEN NEI.CFOP_CODIGO = '2.102' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '60' OR NEI.STC_CODIGO = '70')  THEN 1
-											  WHEN NEI.CFOP_CODIGO = '1.102' AND (NEI.STC_CODIGO = '10' OR NEI.STC_CODIGO = '60' OR NEI.STC_CODIGO = '70')  THEN 1
-												ELSE 0
-										  END
-										)
-										
-									 -- LIMITANDO A BUSCA À 2 ANOS. ALTERAÇÃO FEITA APÓS CONVERSA COM KARINA (16/09/2022)
-										AND 1 = CASE WHEN MONTHS_BETWEEN (P_DATA_VENDA, NFE.DT_FATO_GERADOR_IMPOSTO) < 24 THEN 1 ELSE 0 END
-										
-										-- CONSIDERAR SOMENTE SE TIVER VALOR DE ST. -- INCLUIDO DIA 18/05/2023 APÓS CONVERSA COM KARINA. O RELATÓRIO ESTAVA TENDO NOTAS SEM VALOR DE ST.
-										
-										AND 1 =  CASE
-													  WHEN (NEI.CFOP_CODIGO = '1.409' OR NEI.CFOP_CODIGO = '2.409')   THEN 1 -- incluido dia 01-06-2023. Essas notas não estavam indo para o relatório. Deve ir independente se teve valor de ST
-													  WHEN (NVL(NEI.VL_STF,0) > 0 OR NVL(NEI.VL_STF_FRONTEIRA,0) > 0) THEN 1 
-													  ELSE 0 
-												 END
-										
-										AND U.DOF_IMPORT_NUMERO IS NULL
+										(NFE.DOF_IMPORT_NUMERO, NEI.MERC_CODIGO)   = (
+																						SELECT 
+																							DOF_IMPORT_NUMERO, MERC_CODIGO
+																						FROM 
+																						(
+																							SELECT 
+																								NEI2.MERC_CODIGO,NFE2.DOF_IMPORT_NUMERO, MIN(NFE2.DT_FATO_GERADOR_IMPOSTO) MIN_DATA
+																							FROM SYNCHRO.COR_DOF NFE2
+																							INNER JOIN SYNCHRO.COR_IDF NEI2 ON   NEI2.DOF_ID   = NFE2.ID
+																							INNER JOIN SYNCHRO.TBFILIAL F2                            ON  (F2.EST_CODIGO = NFE2.INFORMANTE_EST_CODIGO  OR F2.FILIAL = NFE2.INFORMANTE_EST_CODIGO)
+																							LEFT JOIN USRASSISTENTEFISCAL.EXCEDENTE_MG_ENTRADA_DET U2 ON   NFE2.DOF_IMPORT_NUMERO = U2.DOF_IMPORT_NUMERO AND NEI2.MERC_CODIGO = U2.MERC_CODIGO AND NEI2.IDF_NUM = U2.IDF_NUM 
+																							WHERE 
+																								  F2.FILIAL                       	= P_FILIAL
+																								  AND NEI2.MERC_CODIGO            	= P_MERC_CODIGO
+																								  AND NFE2.DT_FATO_GERADOR_IMPOSTO BETWEEN P_DATA_CORTE AND P_DATA_VENDA
+																								  AND NFE2.CTRL_SITUACAO_DOF      	IN ('N','B')					  
+																								  AND 1 =
+																									(
+																									  CASE
+																										  WHEN NEI2.CFOP_CODIGO = '1.403'                                          										THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '1.910'                                        										THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '1.949' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.403'                                        										THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '1.409'                                        										THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.409' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.910' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.923' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.949' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '3.102' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '70')               			THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '2.102' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '60' OR NEI2.STC_CODIGO = '70')  THEN 1
+																										  WHEN NEI2.CFOP_CODIGO = '1.102' AND (NEI2.STC_CODIGO = '10' OR NEI2.STC_CODIGO = '60' OR NEI2.STC_CODIGO = '70')  THEN 1
+																											ELSE 0
+																									  END
+																									)
+																									
+																								 -- LIMITANDO A BUSCA À 2 ANOS. ALTERAÇÃO FEITA APÓS CONVERSA COM KARINA (16/09/2022)
+																									AND 1 = CASE WHEN MONTHS_BETWEEN (P_DATA_VENDA, NFE2.DT_FATO_GERADOR_IMPOSTO) < 24 THEN 1 ELSE 0 END
+																									
+																									-- CONSIDERAR SOMENTE SE TIVER VALOR DE ST. -- INCLUIDO DIA 18/05/2023 APÓS CONVERSA COM KARINA. O RELATÓRIO ESTAVA TENDO NOTAS SEM VALOR DE ST.
+																									AND 1 =  CASE
+																												  WHEN (NEI2.CFOP_CODIGO = '1.409' OR NEI2.CFOP_CODIGO = '2.409')   THEN 1 -- incluido dia 01-06-2023. Essas notas não estavam indo para o relatório. Deve ir independente se teve valor de ST
+																												  WHEN (NVL(NEI2.VL_STF,0) > 0 OR NVL(NEI2.VL_STF_FRONTEIRA,0) > 0) THEN 1 
+																												  ELSE 0 
+																											 END
+																								
+																								AND U2.DOF_IMPORT_NUMERO IS NULL
+																								
+																								GROUP BY NEI2.MERC_CODIGO,NFE2.DOF_IMPORT_NUMERO
+																								ORDER BY MIN_DATA
+																						)WHERE ROWNUM < 2
+									)
+									AND U.DOF_IMPORT_NUMERO IS NULL
 								   ORDER BY NFE.DT_FATO_GERADOR_IMPOSTO DESC
                 ) WHERE ROWNUM < 2
 						 	 
